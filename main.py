02a8ae0de92131630d06b16c13de03a548d472a3
qwinox <60475943+qwinox@users.noreply.github.com>
2024-03-11 21:23:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import streamlit as st
 # st.title("Нужно придумать название сюда")
 
 
-st.text_input("Введите длину балки в м:", 0.2, help="Подсказка")
+st.text_input("Введите длину балки в м:", 0.2, help="London is a capital of great britan")
