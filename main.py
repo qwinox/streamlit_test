c02f384ebdc6ffb02b4589c084d81f7a73493d18
qwinox <60475943+qwinox@users.noreply.github.com>
2024-05-05 20:22:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
 
 # st.title("Нужно придумать название сюда")
-
-
 st.text_input("Введите длину балки в м:", help="London is a capital of great britan", placeholder="London")
-
-
 st.image('edit.png', caption='Sunrise by the mountains')
-
-
-
 
 # Создаем новый график
 fig, ax = plt.subplots()
@@ -23,8 +17,17 @@ width = 3
 height = 2
 rectangle = Rectangle((x, y), width, height, edgecolor='r', facecolor='none')
 
+# Создаем круг с центром в (x, y) и радиусом radius
+x = 2
+y = 2
+radius = 1
+circle = Circle((x, y), radius, edgecolor='b', facecolor='none')
+
 # Добавляем прямоугольник на график
 ax.add_patch(rectangle)
+
+# Добавляем круг на график
+ax.add_patch(circle)
 
 # Устанавливаем пределы осей
 plt.xlim(0, 5)
