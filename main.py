b500736c3d96d4e796a0e64621197f2e7557a532
qwinox <60475943+qwinox@users.noreply.github.com>
2024-05-05 20:19:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 # st.title("Нужно придумать название сюда")
 
@@ -7,3 +9,26 @@ st.text_input("Введите длину балки в м:", help="London is a c
 
 
 st.image('edit.png', caption='Sunrise by the mountains')
+
+
+
+
+# Создаем новый график
+fig, ax = plt.subplots()
+
+# Создаем прямоугольник с координатами (x, y), шириной width и высотой height
+x = 1
+y = 1
+width = 3
+height = 2
+rectangle = Rectangle((x, y), width, height, edgecolor='r', facecolor='none')
+
+# Добавляем прямоугольник на график
+ax.add_patch(rectangle)
+
+# Устанавливаем пределы осей
+plt.xlim(0, 5)
+plt.ylim(0, 5)
+
+# Показываем график
+plt.show()
